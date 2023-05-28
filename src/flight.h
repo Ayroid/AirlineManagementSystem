c56dd970ca9083aa8c35dd2910604452f0845ef0
Ayroid <ayushsk0000@gmail.com>
2023-05-29 01:26:25 +0530

    flight.h

---
+++ src/flight.h
@@ -0,0 +1,28 @@
+#ifndef FLIGHT_H
+#define FLIGHT_H
+
+#include <string>
+#include <vector>
+#include "passenger.h"
+
+class Flight {
+public:
+    Flight(const std::string& flightNumber, const std::string& airline, const std::string& source,
+           const std::string& destination, const std::string& date, const std::string& time, int capacity);
+
+    void addPassenger(const Passenger& passenger);
+    void displayFlightDetails() const;
+    void displayPassengerDetails() const;
+
+private:
+    std::string flightNumber;
+    std::string airline;
+    std::string source;
+    std::string destination;
+    std::string date;
+    std::string time;
+    int capacity;
+    std::vector<Passenger> passengers;
+};
+
+#endif
